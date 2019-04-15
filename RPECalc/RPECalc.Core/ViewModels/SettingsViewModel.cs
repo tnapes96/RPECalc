0ae539732823fd786eff24aa900794ef182937b2
tnapes96 <48109379+tnapes96@users.noreply.github.com>
2019-04-15 16:22:59 -0400

    Add Xamarin.Essentials NuGet

--- RPECalc/RPECalc.Core/ViewModels/SettingsViewModel.cs
+++ RPECalc/RPECalc.Core/ViewModels/SettingsViewModel.cs
@@ -2,11 +2,22 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Essentials;
 
 namespace RPECalc.Core.ViewModels
 {
     public class SettingsViewModel : MvxViewModel
     {
-        
+
+
+        public string WeightUnit
+        {
+            get => Preferences.Get(nameof(WeightUnit), "lbs");
+            set
+            {
+                Preferences.Set(nameof(WeightUnit), value);
+                RaisePropertyChanged(nameof(WeightUnit));
+            }
+        }
     }
 }
